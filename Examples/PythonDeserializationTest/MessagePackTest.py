e564e7e45a4a2b8e140e176d8e2d46ef68d641e6
sergioloff <sergioloff@gmail.com>
2021-04-24 19:32:15 +0100

    fixed licence info

--- Examples/PythonDeserializationTest/MessagePackTest.py
+++ Examples/PythonDeserializationTest/MessagePackTest.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2021, Sergio Loff
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 import msgpack
 
 with open("TestStructSmall1_msgPack.dat", "rb") as data_file:
--- Examples/PythonDeserializationTest/ProtoToPythonTest.py
+++ Examples/PythonDeserializationTest/ProtoToPythonTest.py
@@ -1,4 +1,10 @@
+# Copyright (c) 2021, Sergio Loff
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import google.protobuf as pb
+
 # run protoc on console in order to generate the python proxy:
 # protoc -I="<path to source proto file dir>" --python_out="<dest dir>" "<source proto file>.proto"
 import TestStructSmall1_pb2
