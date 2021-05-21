4798c2a6b45a72f7988abfb92697a6e7b50ace56
sergioloff <sergioloff@gmail.com>
2021-05-21 19:28:50 +0100

    i hate git

--- ScratchPad/Program.cs
+++ ScratchPad/Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.IO;
-using slStreamUtilsMessagePackBenchmark;
-using MessagePack;
-using System.Threading;
-using System.Linq;
-using System.Diagnostics;
-using slStreamUtilsMessagePack;
 using System.Threading.Tasks;
-using slStreamUtils;
-using slStreamUtils.Streams;
 
 namespace ScratchPad
 {
